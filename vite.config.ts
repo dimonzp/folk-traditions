ffdff9dd19a15e8527f592cd6e0fd8acfaa2a58e
Dmytro Khekalo <dimon.messages@gmail.com>
2026-05-20 20:36:35 +0200

    GH-pages

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  base: '/folk-traditions/',
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
